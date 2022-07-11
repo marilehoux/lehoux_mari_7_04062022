495905c20abb563f48ab2714595c9966f09a5091
marilehoux <mari.lehoux@gmail.com>
2022-07-11 15:19:17 +0200

    première requête api ok

--- backend/app.js
+++ backend/app.js
@@ -2,30 +2,30 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-//require ('dotenv').config();
+//require ('dotenv').config(); Important!! Requis pour utiliser les variables d'environenement
 
 const userRoutes = require('./routes/user');
 const postRoutes = require('./routes/post');
 
 //modif connexion
-
-mongoose.connect(process.env.CONNECT,
-{ useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !? yes'))
-  .catch((e) => console.log(e));
-  // console.log('Connexion à MongoDB échouée !'));
-
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
+// connexion à la base de données avec la vairable d'environnement pour masquer le mdp notamment sur dépôt git
+mongoose.connect(process.env.CONNECT,
+{ useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !? yes'))
+  .catch(() => console.log('la connexion à la base de données a echouée!'));
+
+  app.use(express.json());
 
 //app.use('/api/post', postRoutes);
-//app.use('/api/auth', userRoutes);
-
+app.use('/api/auth', userRoutes);
+/* ancien serveur pour la mise en place
 app.use((req, res, next) => {
   console.log('Requête reçue !');
   next();
@@ -44,5 +44,5 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   console.log('Réponse envoyée avec succès !');
 });
-
+*/
 module.exports = app;
---
+++ backend/routes/post.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+/*
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');//à installer
+
+const postCtrl = require('../controllers/post');
+
+router.get('/', auth, postCtrl.getAllPost); // renvoie le tableau de tous les posts
+router.post('/', auth, multer, postCtrl.createPost); // capture et enregistre un post texte +image
+
+router.put('/:id', auth, multer, postCtrl.modifyPost); // met à jour le post avec l'id fourni
+router.delete('/:id', auth, multer, postCtrl.deletePost); // supprime le post avec l'id fourni
+
+router.post('/:id/like', auth, postCtrl.like); // enregistre un like id userId fourni
+*/
+module.exports = router;
+
+
