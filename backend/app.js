5576a99e38d2efdc22adac9c01e746a513fe93b7
marilehoux <mari.lehoux@gmail.com>
2022-07-12 08:55:28 +0200

    mise a jour sur dell

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,15 @@ const app = express();
 //require ('dotenv').config();
 
 const userRoutes = require('./routes/user');
-const postRoutes = require('./routes/post');
+//const postRoutes = require('./routes/post');
 
 //modif connexion
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 mongoose.connect(process.env.CONNECT,
 { useNewUrlParser: true,
@@ -16,16 +22,11 @@ mongoose.connect(process.env.CONNECT,
   .catch((e) => console.log(e));
   // console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+
 
 //app.use('/api/post', postRoutes);
-//app.use('/api/auth', userRoutes);
-
+app.use('/api/auth', userRoutes);
+/*
 app.use((req, res, next) => {
   console.log('Requête reçue !');
   next();
@@ -44,5 +45,5 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   console.log('Réponse envoyée avec succès !');
 });
-
+*/
 module.exports = app;
--- backend/models/user.js
+++ backend/models/user.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-    email:{type: String, required: true, unique: true},
-    password: {type: String, required: true,},
+    email:{ type: String, required: true, unique: true},
+    password: { type: String, required: true,},
     level: {type: Number, required: true, default: 0}
 });
 
 userSchema.plugin(uniqueValidator);
 
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model('User', userSchema);
